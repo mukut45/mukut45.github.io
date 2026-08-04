114834465734b765cb37c5ae56009e3c95707082
Mukut Dutta <mukutdutta45@icloud.com>
2026-08-04 15:35:51 +0530

    reducing the paragraph effect radious

--- script.js
+++ script.js
@@ -157,7 +157,7 @@
       document.querySelector(".scramble-paragraph");
   const paragraphCharacters =
       "<$%@(){}[]*>!#";
-  const paragraphEffectRadius = 80;
+  const paragraphEffectRadius = 50;
   // const paragraphScrambleSpeed = 0.8;
   const paragraphEffectDuration = 1500;
   let paragraphLetters = [];
